72f03f6f7ac5ab8448599d72cc3808cd4d28d81b
hajar fatheddine <fatheddinehajar@gmail.com>
2022-12-30 12:17:45 +0100

    gateway and customer services

---
+++ gateway-service/src/main/java/com/gateway/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,32 @@
+package com.gateway.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	//@Bean
+    /* RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route((r)->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route((r)->r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
+                .build();
+    }
+    */
+
+	// à chaque fois ce service recoit une requete regarde l'URL c'est la où tu va trouver le nom du microservice
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient discoveryClient, DiscoveryLocatorProperties properties){
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient,properties);
+	}
+}
